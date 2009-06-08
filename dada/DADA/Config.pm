fe735e8b987ae82f095a57aa57aa4cae10d027ca
justingit <perl@justinsimoni.com>
2009-06-08 14:38:02 -0600

    $VER change. I've also, bizarrely, changed the, "enable_ass_subscribe" variable from, "1" to, "0"

--- dada/DADA/Config.pm
+++ dada/DADA/Config.pm
@@ -4092,7 +4092,7 @@ encrypted.
 
 # List CP -> Options
 	enable_fckeditor                => 1, 
-	enable_mass_subscribe           => 1, 
+	enable_mass_subscribe           => 0, 
     
 
 # Send me the list password.    
@@ -4438,7 +4438,7 @@ and to say that you've got the freshest tools on the Web.
 
 
 $VERSION = 3.1.0; 
-$VER     = '3.1.0 - alpha 1 03/14/08';
+$VER     = '3.1.0 - alpha 05/15/08';
 
 #
 #
