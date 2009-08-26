33a2312cb6d647bf193ca838141f214efd6080f4
justingit <perl@justinsimoni.com>
2009-08-25 23:01:58 -0600

    Oops. I guess "create if not exists table_name" doesn't work for postgresql? Sigh. Will not use it, then!

--- dada/extras/SQL/postgres_schema.sql
+++ dada/extras/SQL/postgres_schema.sql
@@ -1,10 +1,10 @@
-CREATE TABLE IF NOT EXISTS dada_settings (
+CREATE TABLE dada_settings (
 list                             varchar(16),
 setting                          varchar(64),
 value                            text
 );
 
-CREATE TABLE IF NOT EXISTS dada_subscribers (
+CREATE TABLE dada_subscribers (
 email_id                         serial,
 email                            text,
 list                             varchar(16),
@@ -13,7 +13,7 @@ list_status                      char(1)
 );
 
 
-CREATE TABLE IF NOT EXISTS dada_profile (
+CREATE TABLE dada_profile (
 	profile_id			         serial,
 	email                        varchar(320) not null UNIQUE,
 	password                     text,
@@ -23,13 +23,13 @@ CREATE TABLE IF NOT EXISTS dada_profile (
 	activated                    char(1)
 );
 
-CREATE TABLE IF NOT EXISTS dada_profile_fields (
+CREATE TABLE dada_profile_fields (
 	fields_id			         serial,
 	email                        varchar(320) not null UNIQUE
 );
 
 
-CREATE TABLE IF NOT EXISTS dada_profile_fields_attributes ( 
+CREATE TABLE dada_profile_fields_attributes ( 
 
 attribute_id 				serial,
 field                       varchar(320) UNIQUE,
@@ -47,7 +47,7 @@ fallback_value              varchar(320)
 
 	
 
-CREATE TABLE IF NOT EXISTS dada_archives (
+CREATE TABLE dada_archives (
 list                          varchar(32),
 archive_id                    varchar(32),
 subject                       text,
@@ -56,14 +56,14 @@ format                        text,
 raw_msg                       text
 );
 
-CREATE TABLE IF NOT EXISTS dada_bounce_scores (
+CREATE TABLE dada_bounce_scores (
 id                            serial, 
 email                         text, 
 list                          varchar(16),
 score                         int4 
 ); 
 
-CREATE TABLE IF NOT EXISTS dada_clickthrough_urls (
+CREATE TABLE dada_clickthrough_urls (
 url_id  serial,
 redirect_id varchar(16), 
 msg_id text, 
@@ -72,7 +72,7 @@ url text
 
 
 
-CREATE TABLE IF NOT EXISTS dada_sessions (
+CREATE TABLE dada_sessions (
     id CHAR(32) NOT NULL PRIMARY KEY,
     a_session BYTEA NOT NULL
 );
