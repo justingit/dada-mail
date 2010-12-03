0bc91a7af16289af353aa79785c252b9b2d39b41
justingit <perl@justinsimoni.com>
2010-12-03 16:45:07 -0700

    Moving over this tiny bugfix, so I can start using it, live

--- dada/DADA/App/FormatMessages.pm
+++ dada/DADA/App/FormatMessages.pm
@@ -1484,7 +1484,7 @@ sub unsubscription_confirmationation {
 	    die "no -str! $!" if !exists( $args->{-str} );
 	    #die "no type! $!" if !exists( $args->{-type} );
 
-	    if ( $self->can_find_sub_confirm_link( { -str => $args->{-str} } ) ) {
+	    if ( $self->can_find_unsub_confirm_link( { -str => $args->{-str} } ) ) {
 	        # ...
 	    }
 	    else {
