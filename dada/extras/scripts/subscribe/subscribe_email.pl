8fe286a6357c44bc2df623a865ae3bcaf9b96c99
justingit <perl@justinsimoni.com>
2011-01-27 23:07:00 -0700

    Renamed.

---
+++ dada/extras/scripts/subscribe/subscribe_email.pl
@@ -0,0 +1,102 @@
+#!/usr/bin/perl 
+
+package subscribe_email;
+use lib qw(./ ./DADA/perllib);
+
+use DADA::Config;
+use DADA::App::Subscriptions;
+use DADA::App::Guts;
+use CGI;
+use Getopt::Long;
+
+my $verbose = 0;
+my $email;
+my $list;
+
+GetOptions(
+    "email=s" => \$email,
+    "list=s"  => \$list,
+    "verbose" => \$verbose,
+);
+
+__PACKAGE__->run()
+  unless caller();
+
+sub run {
+	
+	my $status = 0; 
+	my $errors = {}; 
+	
+	use DADA::App::Guts; 
+	if(check_if_list_exists(-List => $list) == 0){ 
+		
+		#return [0, {invalid_list => 1}];
+		if($verbose == 1){ 
+			require Data::Dumper; 
+			Data::Dumper::Dumper(
+				{ 
+					status => 0, 
+					errors => {
+						invalid_list => 1, 
+					}
+				}
+			); 
+		}
+	}
+
+	require DADA::MailingList::Settings; 
+	my $ls = DADA::MailingList::Settings->new({-list => $list}); 
+
+	require  DADA::MailingList::Subscribers; 
+	my $lh = DADA::MailingList::Subscribers->new(
+				{
+					-list => $list
+				}
+			);
+			
+	# There may be tests that we want to skip: 
+	my $skip_tests = [];
+	# Black listed subscribers by default can subscribe themselves: 
+	if($ls->param('allow_blacklisted_to_subscribe') == 1){  
+       push(@$skip_tests, 'black_listed');  
+	}
+	# We want to at least say there's no problem, but an email message will
+	# be sent about it. 
+	if($ls->param('email_your_subscribed_msg') == 1){  
+ 		push(@$skip_tests, 'subscribed');  
+	}
+	
+ 	my ($sc_status, $sc_errors) = $lh->subscription_check(
+								{
+									-email => $email,
+									-skip  => $skip_tests, 
+								}
+							);
+							
+	if($sc_status == 1){ 
+		
+		require CGI;
+		my $q = new CGI; 
+		   $q->param('f',     's'   );
+		   $q->param('list',  $list ); 
+		   $q->param('email', $email); 
+
+	    require   DADA::App::Subscriptions; 
+	    my $das = DADA::App::Subscriptions->new; 
+
+	   		$r =  $das->subscribe(
+	        {
+	            -cgi_obj     => $q, 
+				-html_output => 0,
+	        }
+	    ); 
+	}
+	if($verbose == 1){ 
+		require Data::Dumper; 
+		Data::Dumper::Dumper(
+			{ 
+				status => $sc_status, 
+				errors => $sc_errors,
+			}
+		); 
+	}}
---
+++ dada/extras/scripts/subscribe/subscribe_soap_client.cgi
@@ -0,0 +1,57 @@
+#!/usr/bin/perl 
+
+use CGI::Carp qw(fatalsToBrowser); 
+
+# There should be a test to make sure SOAP::Lite is installed. 
+use SOAP::Lite;
+
+use CGI qw(:standard); 
+
+my $proxy     = 'http://localhost/cgi-bin/dada/extras/scripts/suscribe/subscribe_soap_server.cgi'; 
+my $namespace = 'DadaMail';
+
+
+my $email     = 'user@example.com'; 
+my $list      = 'listshortname';
+
+
+print header(); 
+
+my $soap = SOAP::Lite 
+	-> uri('urn:' . $namespace)
+  	-> proxy($proxy);
+
+  my $result = $soap->subscribe($list, $email);
+
+print "<pre>\n"; 
+print '	* Email: ' . $email . "\n"; 
+print '	* List: '  . $list  . "\n\n"; 
+
+unless ($result->fault) {
+	my $return = $result->result();
+
+	require Data::Dumper; 
+	print Data::Dumper::Dumper($return); 
+	
+	my $check  = $return->[0]; 
+
+
+	print "Subscription Check: $check\n"; 
+
+	my $errors = $return->[1]; 
+
+
+	if(keys %$errors){ 
+		print "Errors: \n"; 
+	foreach(keys %$errors){ 
+			print "	* Error: $_\n";
+		}
+	}
+
+} else {
+	print join ', ', 
+	$result->faultcode, 
+	$result->faultstring;
+}
+
+print "done.\n";
