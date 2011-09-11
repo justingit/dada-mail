85888e07f5e442ebefe2c081c17e1a1efd2b8829
justingit <perl@justinsimoni.com>
2011-09-10 21:02:04 -0600

    Closes #193 Wrong Manual version used in v4.7.0 (defaults to 4.6.0)

--- dada/DADA/Config.pm
+++ dada/DADA/Config.pm
@@ -578,7 +578,7 @@ The default setting is the online version that we provide - but it is a paid ser
 
 =cut
 
-$HELP_LINKS_URL ||= 'http://dadamailproject.com/pro_dada/4.6.0';
+$HELP_LINKS_URL ||= 'http://dadamailproject.com/pro_dada/4.7.0';
 
 
 =pod
