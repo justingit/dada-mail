21587167dfe13d148d16bf79ac5980724d78bf2c
justingit <perl@justinsimoni.com>
2011-09-17 13:34:08 -0600

    Closes #205 Tracker: Tracking message opens enabled by default

--- dada/DADA/Config.pm
+++ dada/DADA/Config.pm
@@ -4801,7 +4801,7 @@ encrypted.
 # Tracker
 
 	clickthrough_tracking                               => 1,
-	enable_open_msg_logging                             => 1, 
+	enable_open_msg_logging                             => 0, 
 	enable_subscriber_count_logging                     => 1, 
 	enable_bounce_logging                               => 1,
 	enable_forward_to_a_friend_logging                  => 1, 
