8bf40cbb03046152e3d42d45a5068fe513327705
justingit <perl@justinsimoni.com>
2012-05-01 23:19:42 -0600

    Little tweak in the debug line

--- dada/DADA/App/POP3Tools.pm
+++ dada/DADA/App/POP3Tools.pm
@@ -83,7 +83,7 @@ sub mail_pop3client_login {
        $pop->Connect() >= 0 || die $pop->Message();
        
        if($pop->Count == -1){ 
-            $r .= "\tConnection to '" . $args->{server} . "' wasn't successful: " . $pop->Message();
+            $r .= "\tConnection to '" . $args->{server} . "' wasn't successful: " . $pop->Message() . "\n";
        	   return ( undef, 0, $r );
 	    
 		}
