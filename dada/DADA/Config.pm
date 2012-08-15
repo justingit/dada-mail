27f69da2f271ecc5e78f2d6e7d8dc2e292860035
justingit <perl@justinsimoni.com>
2012-08-15 13:49:20 -0600

    I'm turning auto-tracking of clickthrough links ON!

--- dada/DADA/Config.pm
+++ dada/DADA/Config.pm
@@ -4885,7 +4885,7 @@ encrypted.
 	enable_view_archive_logging                         => 1, 	
 	tracker_record_view_count                           => 10, 
 	tracker_clean_up_reports                            => 1, 
-	tracker_auto_parse_links                            => 0, 
+	tracker_auto_parse_links                            => 1, 
 	tracker_show_message_reports_in_mailing_monitor     => 0, 
 
 # Bounce Handler
