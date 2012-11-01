e664272e66e1c9f6d4a7e4ac96d4bab41496f04e
justingit <perl@justinsimoni.com>
2012-11-01 16:53:14 -0600

    start of 5.2.1 to 5.2.2 in everything

--- dada/DADA/Config.pm
+++ dada/DADA/Config.pm
@@ -5178,8 +5178,8 @@ and to say that you've got the freshest tools on the Web.
 
 =cut
 
-$VERSION = 5.2.1; 
-$VER     = '5.2.1 Stable - 8/17/12';
+$VERSION = 5.2.2; 
+$VER     = '5.2.2 Stable - 11/01/12';
 
 #
 #
