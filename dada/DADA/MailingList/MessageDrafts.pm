1b2a2b0de7740ed1840809c3f80c77cb92454bc1
justingit <perl@justinsimoni.com>
2013-09-13 22:03:30 -0600

    This small bit makes things work,

--- dada/DADA/MailingList/MessageDrafts.pm
+++ dada/DADA/MailingList/MessageDrafts.pm
@@ -9,10 +9,9 @@ BEGIN {
 	my $type = $DADA::Config::BACKEND_DB_TYPE;
 	if($type eq 'SQL'){ 
 			$backend = 'baseSQL';
-#	}
-#	elsif($type eq 'Default'){ 
-#		$backend = 'Db'; 
-
+	}
+	elsif($type eq 'Default'){ 
+		$backend = 'Default'; 
 	}
 	else { 
 		die "Unknown \$BACKEND_DB_TYPE: '$type' Supported types: 'SQL'"; 
