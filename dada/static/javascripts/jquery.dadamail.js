e3580e63275b83ef83155033dcd3925721bfb951
justingit <perl@justinsimoni.com>
2013-09-23 18:32:55 -0600

    Update to make this all work.

--- dada/static/javascripts/jquery.dadamail.js
+++ dada/static/javascripts/jquery.dadamail.js
@@ -18,7 +18,9 @@
 					list:    opts.list
 				},
 				success: function(data) {
-					$this.html(data);					
+					$this.html(data);	
+					control_the_form(); 
+									
 				},
 				error: function() {
 					console.log('something is wrong with, "SubscriptionForm"');
@@ -30,94 +32,179 @@
 		DadaMailURL: "../../../dada/mail.cgi"
 	};
 
+	function control_the_form() { 
+		/* We're in control, now: */
+		$("body").on("submit", "#jquery_subscription_form", function(event) {
+			event.preventDefault();
+		});
+		$("submit", "#jquery_subscription_form").bind("keypress", function (e) {
+		    if (e.keyCode == 13) {
+		        return false;
+		    }
+		});
+		
+		$('body').on('click', '#subscribe_button', function(event) {
+			
+			var fields = {};
+			
+			$("#subscriber_fields :input").each(function() {
+				fields[this.name] = this.value;
+			}); 
+							
+			$.ajax({
+				url: $("#jquery_subscription_form").attr("action") + '/json/subscribe',
+				type: "POST",
+				dataType: "jsonp",
+				cache: false,
+				data: JSON.stringify(
+					{ 
+						list:  $("#list").val(),
+						email: $("#email").val(),
+						fields: fields
+					 }
+				),
+			    contentType: "application/json; charset=UTF-8",
+				success: function(data) {
+					console.log('data:' + JSON.stringify(data)); 
+					var html = ''; 
+					if(data.status === 0){ 
+						var already_sent_sub_confirmation = 0; 
+												
+						$.each(data.errors, function(index, value) {
+							console.log(index + ': ' + value);
+						});
+						
+						$.each(data.error_descriptions, function(index, value) {
+							html += value;
+						});
+					}
+					else { 
+						html += data.success_message;
+					}
+					
+					if(typeof data.redirect_required === 'undefined') {
+						if(data.redirect.using === 1) {
+							if(data.redirect.using_with_query === 1){ 
+								window.location.href = data.redirect.url + '?' + data.redirect.query; 
+							}
+							else { 
+								window.location.href = data.redirect.url;
+							}
+						}
+						else { 
+							$.colorbox({
+								html: html,
+								maxHeight: 480,
+								maxWidth: 649,
+								opacity: 0.50
+							}); 
+						}
+					}
+					else { 
+						/* Success, or Error: it may not be something we can work with: */
+						//alert(data.redirect_required); 
+						window.location.href = data.redirect.url;
+					}
+				},
+				error: function(xhr, ajaxOptions, thrownError) {
+					console.log('status: ' + xhr.status);
+					console.log('thrownError:' + thrownError);
+					$.colorbox({
+						html: '<h1>Apologies,</h1><p>An error occured while processing your request. Please try again in a few minutes.</p>',
+						opacity: 0.50
+					});
+				}
+			}); 
+		}); 
+		
+		
+	}
 
 	function debug($obj) {
 		if (window.console && window.console.log) {
-			window.console.log("Yadda Yadda");
+			/* window.console.log("Yadda Yadda"); */
 		}
 	};
 	
-	/* We're in control, now: */
-	$("body").on("submit", "#jquery_subscription_form", function(event) {
-		event.preventDefault();
-	});
-	$("submit", "#jquery_subscription_form").bind("keypress", function (e) {
-	    if (e.keyCode == 13) {
-	        return false;
-	    }
-	});
 })(jQuery);
 
 
 $(document).ready(function() {
-
-
-	/* We're in control, now: */
-	$("body").on("submit", "#jquery_subscription_form", function(event) {
-		event.preventDefault();
-	});
-	$("submit", "#jquery_subscription_form").bind("keypress", function (e) {
-	    if (e.keyCode == 13) {
-	        return false;
-	    }
-	});
-	
-	$('body').on('click', '#subscribe_button', function(event) {
-		var fields = {};
-
-		$("#subscriber_fields :input").each(function() {
-			fields[this.name] = this.value;
-		}); 
+		
+		$('body').on('click', '#subscribe_button', function(event) {
 			
-		$.ajax({
-			url: $("#jquery_subscription_form").attr("action") + '/json/subscribe',
-			type: "POST",
-			dataType: "jsonp",
-			cache: false,
-			data: JSON.stringify(
-				{ 
-					list:  $("#list").val(),
-					email: $("#email").val(),
-					fields: fields
-				 }
-			),
-		    contentType: "application/json; charset=utf-8",
-			success: function(data) {				
-				console.log('data:' + JSON.stringify(data));
-				var html = ''; 
-				if(data.status === 0){ 
-					/* Uh uh.*/
-					html += '<h1>Problems with your request:</h1>'; 
-					html += '<ul>'; 
-					$.each(data.errors, function(index, value) {
-						console.log(index + ': ' + value);
+			var fields = {};
+			
+			$("#subscriber_fields :input").each(function() {
+				fields[this.name] = this.value;
+			}); 
+							
+			$.ajax({
+				url: $("#jquery_subscription_form").attr("action") + '/json/subscribe',
+				type: "POST",
+				dataType: "jsonp",
+				cache: false,
+				data: JSON.stringify(
+					{ 
+						list:  $("#list").val(),
+						email: $("#email").val(),
+						fields: fields
+					 }
+				),
+			    contentType: "application/json; charset=UTF-8",
+				success: function(data) {
+					console.log('data:' + JSON.stringify(data)); 
+					var html = ''; 
+					if(data.status === 0){ 
+						var already_sent_sub_confirmation = 0; 
+												
+						$.each(data.errors, function(index, value) {
+							console.log(index + ': ' + value);
+						});
+						
+						$.each(data.error_descriptions, function(index, value) {
+							html += value;
+						});
+					}
+					else { 
+						html += data.success_message;
+					}
+					
+					if(typeof data.redirect_required === 'undefined') {
+						if(data.redirect.using === 1) {
+							if(data.redirect.using_with_query === 1){ 
+								window.location.href = data.redirect.url + '?' + data.redirect.query; 
+							}
+							else { 
+								window.location.href = data.redirect.url;
+							}
+						}
+						else { 
+							$.colorbox({
+								html: html,
+								maxHeight: 480,
+								maxWidth: 649,
+								opacity: 0.50
+							}); 
+						}
+					}
+					else { 
+						/* Success, or Error: it may not be something we can work with: */
+						//alert(data.redirect_required); 
+						window.location.href = data.redirect.url;
+					}
+				},
+				error: function(xhr, ajaxOptions, thrownError) {
+					console.log('status: ' + xhr.status);
+					console.log('thrownError:' + thrownError);
+					$.colorbox({
+						html: '<h1>Apologies,</h1><p>An error occured while processing your request. Please try again in a few minutes.</p>',
+						opacity: 0.50
 					});
-					$.each(data.error_descriptions, function(index, value) {
-						html += '<li>' + value + '</li>';
-					});
-					html += '</ul>'; 
 				}
-				else { 
-					html += '<h1>Request Successful!:</h1>'; 
-					html += '<p>Your Subscription Request was Successful!</p>'; 
-				}
-	
-				/* html += '<code>' + JSON.stringify(data) + '</code>' */
-	
-				$.colorbox({
-					html: html,
-					opacity: 0.50
-				});
-			},
-			error: function(xhr, ajaxOptions, thrownError) {
-				console.log('status: ' + xhr.status);
-				console.log('thrownError:' + thrownError);
-				$.colorbox({
-					html: '<h1>Apologies,</h1><p>An error occured while processing your request. Please try again in a few minutes.</p>',
-					opacity: 0.50
-				});
-			}
+			}); 
 		}); 
-	}); 
+//	} 
+
 }); 
 
