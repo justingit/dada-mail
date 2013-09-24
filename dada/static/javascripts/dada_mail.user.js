c15a28da8aea54e3a607c80798335da3b1ebfd3b
justingit <perl@justinsimoni.com>
2013-09-24 17:01:32 -0600

    Remove the demo, case well - it's live now.

--- dada/static/javascripts/dada_mail.user.js
+++ dada/static/javascripts/dada_mail.user.js
@@ -17,93 +17,10 @@ $(document).ready(function() {
 		});
 	}
 	
-	if ($("#ajax_subscribe_form_demo").length) {
-		
-		/* We're in control, now: */
-		$("body").on("submit", "#ajax_subscribe_form_demo", function(event) {
-			event.preventDefault();
-		});
-		$("submit", "#ajax_subscribe_form_demo").bind("keypress", function (e) {
-		    if (e.keyCode == 13) {
-		        return false;
-		    }
-		});
-		
-		$('body').on('click', '#subscribe_button', function(event) {
-			
-			var fields = {};
-			
-			$("#subscriber_fields :input").each(function() {
-				fields[this.name] = this.value;
-			}); 
-							
-			$.ajax({
-				url: $("#ajax_subscribe_form_demo").attr("action") + '/json/subscribe',
-				type: "POST",
-				dataType: "jsonp",
-				cache: false,
-				data: JSON.stringify(
-					{ 
-						list:  $("#list").val(),
-						email: $("#email").val(),
-						fields: fields
-					 }
-				),
-			    contentType: "application/json; charset=UTF-8",
-				success: function(data) {
-					console.log('data:' + JSON.stringify(data)); 
-					var html = ''; 
-					if(data.status === 0){ 
-						var already_sent_sub_confirmation = 0; 
-												
-						$.each(data.errors, function(index, value) {
-							console.log(index + ': ' + value);
-						});
-						
-						$.each(data.error_descriptions, function(index, value) {
-							html += value;
-						});
-					}
-					else { 
-						html += data.success_message;
-					}
-					
-					if(typeof data.redirect_required === 'undefined') {
-						if(data.redirect.using === 1) {
-							if(data.redirect.using_with_query === 1){ 
-								window.location.href = data.redirect.url + '?' + data.redirect.query; 
-							}
-							else { 
-								window.location.href = data.redirect.url;
-							}
-						}
-						else { 
-							$.colorbox({
-								html: html,
-								maxHeight: 480,
-								maxWidth: 649,
-								opacity: 0.50
-							}); 
-						}
-					}
-					else { 
-						/* Success, or Error: it may not be something we can work with: */
-						//alert(data.redirect_required); 
-						window.location.href = data.redirect.url;
-					}
-				},
-				error: function(xhr, ajaxOptions, thrownError) {
-					console.log('status: ' + xhr.status);
-					console.log('thrownError:' + thrownError);
-					$.colorbox({
-						html: '<h1>Apologies,</h1><p>An error occured while processing your request. Please try again in a few minutes.</p>',
-						opacity: 0.50
-					});
-				}
-			}); 
-		}); 
-	} 
-	
+	if($("#modal_subscribe_form").length) { 
+		$('#modal_subscribe_form').DadaMail({targetForm: 'subscription_form'});
+		$('#modal_subscribe_form').DadaMail('Modal');
+	}
 	
 	if ($("#create_new_list").length) {
 		
