857bfe17baf9092d3670054f8a7758d8a2bf7492
justingit <perl@justinsimoni.com>
2014-07-01 17:32:40 -0600

    Closes #449

--- dada/static/javascripts/dada_mail.js
+++ dada/static/javascripts/dada_mail.js
@@ -2171,6 +2171,11 @@ function bridge_setup_list_email_type_params() {
 }
 
 function plugins_bridge_test_pop3() {
+	
+	var use_ssl = 0;
+	if($("#discussion_pop_use_ssl").prop("checked") === true){ 
+		use_ssl = 1; 
+	}
 	$.colorbox({
 		top: 0,
 		fixed: true,
@@ -2185,7 +2190,7 @@ function plugins_bridge_test_pop3() {
 			username: $("#discussion_pop_username").val(),
 			password: $("#discussion_pop_password").val(),
 			auth_mode: $("#discussion_pop_auth_mode option:selected").val(),
-			use_ssl: $("#discussion_pop_use_ssl").prop("checked")
+			use_ssl: use_ssl
 		}
 	});
 }
