005ef3ba569c7c7eca56d7c651771ffa3af08585
justingit <perl@justinsimoni.com>
2015-11-16 15:46:05 -0700

    Trying to get the Send a Message/URL buttons to float, and be centered - this is a lot better than before.

--- dada/static/javascripts/dada_mail.js
+++ dada/static/javascripts/dada_mail.js
@@ -106,13 +106,14 @@ jQuery(document).ready(function($){
 		var stickyHeader = $('#buttons').offset().top;
         $(window).scroll(function(){
             if( $(window).scrollTop() > stickyHeader && $('#buttons').width() >= 640) {
-                    $('#buttons').css({position: 'fixed', top: '0px'});	
+                    //$('#buttons').css({position: 'fixed', top: '0px'});	
 					$('#buttons').addClass('floating_panel');				
             } else {
-                    $('#buttons').css({position: 'static', top: '0px'});
+                    //$('#buttons').css({position: 'static', top: '0px'});
 					$('#buttons').removeClass('floating_panel');
             }
         });
+			
 			
 		if($("#additional_email_headers").length){
 			$("#additional_email_headers").hide();
