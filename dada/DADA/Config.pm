ba6284f0bae31d3637bc7374f1ed9454d49836ba
justingit <perl@justinsimoni.com>
2016-12-09 18:04:17 -0700

    10.2.0 -> 10.2.1

--- dada/DADA/Config.pm
+++ dada/DADA/Config.pm
@@ -1665,8 +1665,8 @@ $ATTACHMENT_TEMPFILE //= 0;
 $MAIL_VERP_SEPARATOR //= '-';
 
 
-$VERSION = 10.2.0;
-$VER     = '10.2.0 Stable 12/06/16';
+$VERSION = 10.2.1;
+$VER     = '10.2.1 Stable 12/06/16';
 
 #
 #
