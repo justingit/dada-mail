e8bbb5fa9b84cedbb5a5e1715b67cdc3d7a66eeb
justingit <perl@justinsimoni.com>
2017-01-09 19:39:21 -0700

    This just replied INT4 with INT - they should be exactly the same.

--- dada/extras/SQL/mysql_schema.sql
+++ dada/extras/SQL/mysql_schema.sql
@@ -7,7 +7,7 @@ value                            text
 CREATE INDEX dada_settings_list_index ON dada_settings (list);
 
 CREATE TABLE IF NOT EXISTS dada_subscribers (
-email_id                        int4 not null primary key auto_increment,
+email_id                        int not null primary key auto_increment,
 email                            varchar(80),
 list                             varchar(16),
 list_type                        varchar(64),
@@ -18,7 +18,7 @@ timestamp                       TIMESTAMP DEFAULT NOW()
 CREATE INDEX dada_subscribers_all_index ON dada_subscribers (email(80), list, list_type, list_status, timestamp);
 
 CREATE TABLE IF NOT EXISTS dada_confirmation_tokens ( 
-id INT4 NOT NULL PRIMARY KEY AUTO_INCREMENT,
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 timestamp TIMESTAMP DEFAULT NOW(),
 token varchar(255),
 email varchar(80),
@@ -29,7 +29,7 @@ CREATE INDEX dada_confirmation_tokens_index ON dada_confirmation_tokens (id, tim
 
 
 CREATE TABLE IF NOT EXISTS dada_profiles ( 
-profile_id int4 not null primary key auto_increment,
+profile_id int not null primary key auto_increment,
 email                        varchar(80) not null,
 password                     text(16),
 auth_code                    varchar(64),
@@ -40,13 +40,13 @@ CONSTRAINT UNIQUE (email)
 ) CHARACTER SET utf8 COLLATE utf8_bin;
 
 CREATE TABLE IF NOT EXISTS dada_profile_fields (
-fields_id int4 not null primary key auto_increment,
+fields_id int not null primary key auto_increment,
 email varchar(80) not null,
 CONSTRAINT UNIQUE (email)
 ) CHARACTER SET utf8 COLLATE utf8_bin;
 
 CREATE TABLE IF NOT EXISTS dada_profile_fields_attributes (
-attribute_id int4 not null primary key auto_increment,
+attribute_id int not null primary key auto_increment,
 field                       varchar(80),
 label                       varchar(80),
 fallback_value              text,
@@ -60,7 +60,7 @@ required                    char(1) DEFAULT 0 NOT NULL,
 ) CHARACTER SET utf8 COLLATE utf8_bin;
 
 CREATE TABLE IF NOT EXISTS dada_profile_settings (
-id int4 not null primary key auto_increment,
+id int not null primary key auto_increment,
 email                            varchar(80),
 list                             varchar(16),
 setting                          varchar(64),
@@ -79,10 +79,10 @@ raw_msg                       mediumtext
 CREATE INDEX dada_archives_list_archive_id_index ON dada_archives (list, archive_id);
  
 CREATE TABLE IF NOT EXISTS dada_bounce_scores (
-id                            int4 not null primary key auto_increment,
+id                            int not null primary key auto_increment,
 email                         text, 
 list                          varchar(16),
-score                         int4
+score                         int
 );
 
 CREATE TABLE IF NOT EXISTS dada_sessions (
@@ -91,7 +91,7 @@ CREATE TABLE IF NOT EXISTS dada_sessions (
 );
  
 CREATE TABLE IF NOT EXISTS dada_clickthrough_urls (
-url_id  int4 not null primary key auto_increment, 
+url_id  int not null primary key auto_increment, 
 redirect_id varchar(16), 
 msg_id text, 
 url text
@@ -99,7 +99,7 @@ url text
 
 
 CREATE TABLE IF NOT EXISTS dada_mass_mailing_event_log (
-id INT4 NOT NULL PRIMARY KEY AUTO_INCREMENT,
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 list varchar(16),
 timestamp TIMESTAMP DEFAULT NOW(),
 remote_addr varchar(255), 
@@ -112,7 +112,7 @@ user_agent varchar(255)
 CREATE INDEX dada_mass_mailing_event_log_index ON dada_mass_mailing_event_log (`list`,`remote_addr`, `msg_id`, `event`, timestamp);
 
 CREATE TABLE IF NOT EXISTS dada_clickthrough_url_log (
-id INT4 NOT NULL PRIMARY KEY AUTO_INCREMENT,
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 list varchar(16),
 timestamp TIMESTAMP DEFAULT NOW(),
 remote_addr varchar(255),
@@ -123,7 +123,7 @@ user_agent varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS dada_password_protect_directories (
-id INT4 NOT NULL PRIMARY KEY AUTO_INCREMENT,
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 list varchar(16),
 name text,
 url text,
@@ -134,7 +134,7 @@ default_password text
 );
 
 CREATE TABLE IF NOT EXISTS dada_message_drafts (
-id INT4 NOT NULL PRIMARY KEY AUTO_INCREMENT,
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 list varchar(16),
 created_timestamp TIMESTAMP DEFAULT NOW(),
 last_modified_timestamp TIMESTAMP,
@@ -152,7 +152,7 @@ timestamp INT UNSIGNED NOT NULL
 CREATE INDEX dada_rate_limit_hits_all_index ON dada_rate_limit_hits (user_id, action, timestamp);
 
 CREATE TABLE IF NOT EXISTS dada_email_message_previews (
-id INT4 NOT NULL PRIMARY KEY AUTO_INCREMENT,
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 list varchar(16),
 created_timestamp TIMESTAMP DEFAULT NOW(),
 vars text,
