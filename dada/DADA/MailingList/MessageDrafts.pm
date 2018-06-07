96c6c8a69e44c98736339b47b2ccfad8166b7981
justingit <perl@justinsimoni.com>
2018-06-06 19:04:11 -0600

    Removed!

--- dada/DADA/MailingList/MessageDrafts.pm
+++ dada/DADA/MailingList/MessageDrafts.pm
@@ -5,21 +5,6 @@ use lib qw(
 	../../DADA/perllib
 );
 
-=cut
-BEGIN { 
-	my $type = $DADA::Config::BACKEND_DB_TYPE;
-	if($type eq 'SQL'){ 
-			$backend = 'baseSQL';
-	}
-	elsif($type eq 'Default'){ 
-		$backend = 'Default'; 
-	}
-	else { 
-		die "Unknown \$BACKEND_DB_TYPE: '$type' Supported types: 'SQL'"; 
-	}
-}
-=cut
-
 use base DADA::MailingList::MessageDrafts::baseSQL;
 
 1;
