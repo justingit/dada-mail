13843c7b8a15b0ebdef8e63832394d657c206876
justingit <perl@justinsimoni.com>
2018-07-20 16:08:29 -0600

    $VER change to 11.1.0

--- dada/DADA/Config.pm
+++ dada/DADA/Config.pm
@@ -1707,8 +1707,8 @@ $ATTACHMENT_TEMPFILE //= 0;
 $MAIL_VERP_SEPARATOR //= '-';
 
 
-$VERSION = 11.0.5;
-$VER     = '11.0.5 Stable 07/12/18';
+$VERSION = 11.1.0;
+$VER     = '11.1.0 Stable 07/21/18';
 
 #
 #
