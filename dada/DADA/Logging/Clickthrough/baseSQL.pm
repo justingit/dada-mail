545fff1b2790067c042404ddaf56e9d714073745
justingit <perl@justinsimoni.com>
2019-05-18 18:05:15 -0600

    Updated GeoIP DB has states in a different code, so this translates that into State names

--- dada/DADA/Logging/Clickthrough/baseSQL.pm
+++ dada/DADA/Logging/Clickthrough/baseSQL.pm
@@ -428,6 +428,8 @@ sub _update_profile_fields {
     my $thawed_gip = $self->{ls}->_dd_thaw( $self->{ls}->param('tracker_update_profile_fields_ip_dada_meta') );
     #warn '$thawed_gip:' . $thawed_gip; 
     
+	
+	# <a href="https://www.miyuru.lk/geoiplegacy">Geolocation database of [provider] converted by Miyuru Sankalpa</a> 
     require Geo::IP::PurePerl;
     my $gi = Geo::IP::PurePerl->new($geoip_db);
     my (
@@ -1962,7 +1964,7 @@ sub individual_country_geoip {
 				$city = 'Other'; 
 			}
 			if($country_code eq 'US'){ 
-				$city = $city . ', ' . $region;
+				$city = $city . ', ' . $self->fip_to_state_names($region);
 			}
 			if(!$metro_code){ 
 				$metro_code = 'Unknown'; 
@@ -3101,7 +3103,104 @@ sub remote_addr {
     }
 }
 
+sub fip_to_state_names { 
 
+	my $self = shift; 
+	my $code = shift; 
+	my $lut = { 
+		'01' => 'Alabama',
+		'02' => 'Alaska',
+		'03' => 'American Samoa',
+		'04' => 'Arizona',
+		'05' => 'Arkansas',
+		'06' => 'California',
+		'07' => 'Canal Zone',
+		'08' => 'Colorado',
+		'09' => 'Connecticut',
+		'1' => 'Alabama',
+		'2' => 'Alaska',
+		'3' => 'American Samoa',
+		'4' => 'Arizona',
+		'5' => 'Arkansas',
+		'6' => 'California',
+		'7' => 'Canal Zone',
+		'8' => 'Colorado',
+		'9' => 'Connecticut',
+		'10' => 'Delaware',
+		'11' => 'District of Columbia',
+		'12' => 'Florida',
+		'13' => 'Georgia',
+		'14' => 'Guam',
+		'15' => 'Hawaii',
+		'16' => 'Idaho',
+		'17' => 'Illinois',
+		'18' => 'Indiana',
+		'19' => 'Iowa',
+		'20' => 'Kansas',
+		'21' => 'Kentucky',
+		'22' => 'Louisiana',
+		'23' => 'Maine',
+		'24' => 'Maryland',
+		'25' => 'Massachusetts',
+		'26' => 'Michigan',
+		'27' => 'Minnesota',
+		'28' => 'Mississippi',
+		'29' => 'Missouri',
+		'30' => 'Montana',
+		'31' => 'Nebraska',
+		'32' => 'Nevada',
+		'33' => 'New Hampshire',
+		'34' => 'New Jersey',
+		'35' => 'New Mexico',
+		'36' => 'New York',
+		'37' => 'North Carolina',
+		'38' => 'North Dakota',
+		'39' => 'Ohio',
+		'40' => 'Oklahoma',
+		'41' => 'Oregon',
+		'42' => 'Pennsylvania',
+		'43' => 'Puerto Rico',
+		'44' => 'Rhode Island',
+		'45' => 'South Carolina',
+		'46' => 'South Dakota',
+		'47' => 'Tennessee',
+		'48' => 'Texas',
+		'49' => 'Utah',
+		'50' => 'Vermont',
+		'51' => 'Virginia',
+		'52' => 'Virgin Islands of the U.S.',
+		'53' => 'Washington',
+		'54' => 'West Virginia',
+		'55' => 'Wisconsin',
+		'56' => 'Wyoming',
+		'60' => 'American Samoa',
+		'64' => 'Federated States of Micronesia',
+		'66' => 'Guam',
+		'67' => 'Johnston Atoll',
+		'68' => 'Marshall Islands',
+		'69' => 'Northern Mariana Islands',
+		'70' => 'Palau',
+		'71' => 'Midway Islands',
+		'72' => 'Puerto Rico',
+		'74' => 'U.S. Minor Outlying Islands',
+		'76' => 'Navassa Island',
+		'78' => 'Virgin Islands of the U.S.',
+		'79' => 'Wake Island',
+		'81' => 'Baker Island',
+		'84' => 'Howland Island',
+		'86' => 'Jarvis Island',
+		'89' => 'Kingman Reef',
+		'95' => 'Palmyra Atoll',
+	};
+	if(exists($lut->{$code})){ 
+		return $lut->{$code}; 
+	}
+	else { 
+		return $code; 
+	}
+
+
+}
 
 
 1;
