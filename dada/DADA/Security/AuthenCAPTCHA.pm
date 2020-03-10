673283e1f467ef43dffe8b61712c63857ba940ad
justingit <perl@justinsimoni.com>
2020-03-10 09:49:30 -0600

    This module isn't really used - we call the v2 recaptcha directly.

--- dada/DADA/Security/AuthenCAPTCHA.pm
+++ dada/DADA/Security/AuthenCAPTCHA.pm
@@ -1,21 +1,4 @@
-package DADA::Security::AuthenCAPTCHA;;
-
-# This is all sorts of messed up. 
-use strict; 
-
-use lib qw(../../ ../../DADA ../perllib ./ ../ ../perllib ../../ ../../perllib); 
-
-
-use DADA::Config qw(!:DEFAULT); 
-
-use Carp qw(croak carp); 
-
-if (eval "require require DADA::Security::AuthenCAPTCHA::Google_reCAPTCHA;::$DADA::Config::CAPTCHA_TYPE") {
-    use base "require DADA::Security::AuthenCAPTCHA::Google_reCAPTCHA;::$DADA::Config::CAPTCHA_TYPE";
-    
-}else{ 
-    die("cannot find 'require DADA::Security::AuthenCAPTCHA::Google_reCAPTCHA;::$DADA::Config::CAPTCHA_TYPE', $!");
-}
-
+package DADA::Security::AuthenCAPTCHA;
+# This module just isn't being used for anything. 
 
 1;
