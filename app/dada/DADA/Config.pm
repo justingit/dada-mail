25693c793c146662d28d341df96103ee6903849d
justingit <perl@justinsimoni.com>
2023-09-13 15:57:53 -0600

    $VER change in the source

--- app/dada/DADA/Config.pm
+++ app/dada/DADA/Config.pm
@@ -1816,8 +1816,8 @@ $ATTACHMENT_TEMPFILE //= 0;
 $MAIL_VERP_SEPARATOR //= '-';
 
 
-$VERSION = 11.21.0;
-$VER = 'v11.21.0 stable 2023-08-07';
+$VERSION = 11.22.0;
+$VER = 'v11.22.0 stable 2023-09-18';
 
 #
 #
